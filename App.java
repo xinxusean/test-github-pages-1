cbcf24be4722c962f2da552be408b3363fa53b59
xinxusean <124638768+xinxusean@users.noreply.github.com>
2023-04-30 19:25:02 +0000

    added hello world

---
+++ App.java
@@ -0,0 +1,6 @@
+public static void main(String[] args) {
+    System.out.println("Hello World!");
+    // Scanner scanner = new Scanner(System.in);
+    // String input = scanner.next();
+    // System.out.println("You typed: " + input);
+}
